a9bff70f837614480558dbe3a76f8676a19a9739
Carolina Carranza <cmcc1505@gmail.com>
2021-11-17 03:24:13 -0600

    :sparkles: Add Header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import star from "../../assets/images/star.png";
+
+const Header = () => {
+    return (
+        <div className="flex items-center gap-4 justify-center">
+            <img src={ star } alt="Favorites star" className="w-10 md:w-10"/>
+            <h1 className="text-center text-3xl text-white font-bold uppercase">My favorite sites</h1>
+        </div>
+    )
+}
+
+export default Header
